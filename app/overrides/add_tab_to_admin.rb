b0e0a679cba3432d7e31ac47c2529cd1d305491a
GarPit <garik.piton@gmail.com>
2017-04-17 15:05:37 +0300

    compatible with Spree 3.1

--- app/overrides/add_tab_to_admin.rb
+++ app/overrides/add_tab_to_admin.rb
@@ -1,4 +1,8 @@
-Deface::Override.new(:virtual_path => "layouts/admin",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "add_snippets_tab_to_admin",
-                     :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:snippets) %>")
+                     :insert_bottom => "#main-sidebar",
+                     :text => '<% if can? :admin, Spree::Snippet %>
+                       <ul class="nav nav-sidebar">
+                         <%= tab Spree::Snippet, url: spree.admin_snippets_url, icon: "file", label: Spree::Snippet.model_name.human(count: 1.1) %>
+                       </ul>
+                     <% end %>')
